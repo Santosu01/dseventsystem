f84283b71cec7bf41524bda2135f226700c89736
Marcio Santos <santosu01@gmail.com>
2025-03-04 18:28:00 -0300

    feat: create project

---
+++ src/test/java/com/devsuperior/dseventsystem/DseventsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dseventsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DseventsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
